153a324ce55e0160d842f9f25a1f1fd115fabd70
circular <circular@circulars.dev>
2026-03-08 02:39:54 +1100

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+<div class="p-8 w-full lg:w-1/3 text-left self-center flex flex-col">
+	{@render children()}
+</div>
