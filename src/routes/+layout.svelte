9ec1b7ffaba4dc4c486ddb90b5b32d814c8a9705
circular <circular@circulars.dev>
2026-03-08 23:44:31 +1100

    title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,6 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
-<div class="p-8 w-full lg:w-1/3 text-left self-center flex flex-col">
+<div class="p-8 w-full md:w-3xl text-left self-center flex flex-col">
 	{@render children()}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<p class="w-full bg-ctp-lavender-900">meow</p>
+<div class="w-full text-right flex flex-col gap-2">
+    <h1 class="text-5xl italic font-serif highlight-title">circular</h1>
+    <p>awesome</p>
+    <p>pawesome</p>
+</div>
