9c678dc08851a92817ae139c59bb829fe1933dab
circular <circular@circulars.dev>
2026-04-08 13:54:42 +1000

    this look kinda ass icl

--- src/lib/components/musicStatus.svelte
+++ src/lib/components/musicStatus.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
   import circularCat200Bg from "$lib/assets/circular-cat-200-bg.gif";
-  import RemoteImage from "./remoteImage.svelte";
   import type { LastFMRecentTracks } from "$lib/lastfm";
   
   const websocket = new WebSocket("ws://localhost:3000/ws");
 
-  let error = $state("");
+  let error = $state(false);
 
   let image = $state("");
   let title = $state("loading last.fm...");
@@ -14,7 +13,7 @@
   let playing = $state(false);
 
   websocket.addEventListener("error", (e) => {
-    error = "Failed to connect to last.fm websocket. Please try again later.";
+    error = true;
   });
 
   websocket.addEventListener("message", (event) => {
@@ -28,7 +27,7 @@
 
 </script>
 
-{#if error}
+<!-- {#if error}
   <div
     class="border-ctp-red-200 border-2 bg-opacity-30 p-2 h-32 flex gap-2 relative error-background items-center justify-center"
   >
@@ -39,9 +38,9 @@
     class="text-left border-ctp-subtext1 border-2 bg-opacity-30 p-2 h-32 flex gap-2 relative"
   >
     {#if image}
-      <RemoteImage src={image} alt="Album Art" width="108" height="108" />
+      <img src={image} alt="Album Art" width="108" height="108" />
     {:else}
-      <RemoteImage src={circularCat200Bg} alt="Placeholder" width="108" height="108" />
+      <img src={circularCat200Bg} alt="Placeholder" width="108" height="108" />
     {/if}
     <div class="overflow-hidden flex flex-col">
       {#if playing}
@@ -51,6 +50,31 @@
       {/if}
       <a class="text-lg overflow-auto" href={url}>{title}</a>
       <p class="text-sm text-ctp-subtext0">{artist}</p>
+    </div>
+  </div> 176
+{/if} -->
+
+{#if error}
+  <div class="border-ctp-surface0 border-2 h-44 w-44 p-4 flex disabled-background items-center justify-center">
+    <p class="text-sm text-ctp-overlay0 font-serif text-center italic">music status unavailable</p>
+  </div>
+{:else}
+  <div class="relative h-44 w-44">
+    {#if image}
+      <img src={image} alt="Album Art" width="176" height="176" />
+    {:else}
+      <img src={circularCat200Bg} alt="Placeholder" width="176" height="176" />
+    {/if}
+    <div class="overflow-hidden flex flex-col absolute top-0 left-0">
+      {#if playing}
+        <p class="text-sm font-serif italic">Currently listening to:</p>
+      {:else}
+        <p class="text-sm font-serif italic">Last listened to:</p>
+      {/if}
+    </div>
+    <div class="overflow-hidden flex flex-col absolute bottom-0 right-0 text-right music-overlay">
+      <p class="text-sm text-ctp-subtext0">{artist}</p>
+      <a class="text-lg overflow-auto" href={url}>{title}</a>
     </div>
   </div>
 {/if}
