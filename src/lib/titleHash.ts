a98fa6ddc23cf085a265341f965912d2054f7589
circular <circular@circulars.dev>
2026-05-25 14:29:23 +1000

    props and title hash

---
+++ src/lib/titleHash.ts
@@ -0,0 +1,32 @@
+/*
+  titleHash.ts
+
+  Transforming a string (the title) into a (semi) unique color.
+  This process doesn't have to be perfect, it just has to give one color for one input, and give another color for another.
+*/
+
+const catppuccinColors = [
+  "rgb(245, 224, 220)", // rosewater
+  "rgb(242, 205, 205)", // flamingo
+  "rgb(245, 194, 231)", // pink
+  "rgb(203, 166, 247)", // mauve
+  "rgb(243, 139, 168)", // red
+  "rgb(235, 160, 172)", // maroon
+  "rgb(250, 179, 135)", // peach
+  "rgb(249, 226, 175)", // yellow
+  "rgb(166, 227, 161)", // green
+  "rgb(148, 226, 213)", // teal
+  "rgb(137, 220, 235)", // sky
+  "rgb(116, 199, 236)", // sapphire
+  "rgb(137, 180, 250)", // blue
+  "rgb(180, 190, 254)", // lavender
+]
+
+export function titleHash(title: string) {
+  let sum = 0;
+  for (let i = 0; i < title.length; i++) {
+    sum += title.charCodeAt(i);
+  }
+  sum += title.length; // "foodbot" and "watchcord" collided by pure chance before adding this lmao
+  return catppuccinColors[sum % catppuccinColors.length]
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,5 +13,19 @@
     <MusicStatus />
     <hr class="seperator">
     <h1 class="text-4xl italic font-serif">showcase</h1>
-    <ShowcaseCard />
+    <ShowcaseCard
+      title="watchcord"
+      description="A completely independent Discord client for Apple Watch."
+      icon="https://raw.githubusercontent.com/circularsprojects/watchcord-astro/refs/heads/main/public/watchcord.png"
+      image="https://raw.githubusercontent.com/circularsprojects/watchcord-astro/refs/heads/main/public/watchcord%20home.png"
+      url="https://watchcord.dev"
+    />
+    <ShowcaseCard
+      title="foodbot"
+      description="A discord bot that just sends pictures of food. >2200 servers as of May 2026."
+      icon="foodbot.webp"
+      image="SCR-20260525-mist.png"
+      url="https://discord.com/api/oauth2/authorize?client_id=866362531546398752&permissions=51200&scope=applications.commands%20bot"
+      urlLabel="Invite"
+    />
 </div>
