236c2de6e7a5fa845e109c8ddc67b3a4fc80e16a
circular <circular@circulars.dev>
2026-07-21 12:39:42 +1000

    stripe bg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 
 <div class="w-full text-left flex flex-col gap-4">
     <h1 class="text-5xl italic font-serif highlight-title">circular</h1>
-    <div>
+    <div class="blur-bg">
       <p>semi professional software developer</p>
       <p>diploma of information technology, currently studying towards a bachelor's</p>
       <p>swift student challenge 2024 winner</p>
